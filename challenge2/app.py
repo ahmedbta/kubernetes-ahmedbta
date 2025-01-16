3fbb46a80f2b87eaecf9379df0578db9a7db736f
ahmedbta <120317655+ahmedbta@users.noreply.github.com>
2025-01-16 21:19:06 +0100

    Update app.py

--- challenge2/app.py
+++ challenge2/app.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    # Get current date
     current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Get server hostname
     hostname = socket.gethostname()
 
-    # Replace with your own information
     name = "Ahmed Ben Taleb Ali"
     project_name = "Challenge 1"
     version = "V1"
 
-    # Render all the details
     return f"""
     <h1>{project_name} - {version}</h1>
     <p>Developer: {name}</p>
